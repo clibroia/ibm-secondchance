8dc2df57cf5747088b44bc48d8edd37487a6bc23
clibroia <carmine87.italy@gmail.com>
2024-08-04 12:32:05 +0200

    Lint v2 searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -10,11 +10,11 @@ router.get('/', async (req, res, next) => {
     const collection = db.collection('secondChanceItems')
 
     // Initialize the query object
-    let query = {}
+    const query = {}
 
     // Add the name filter to the query if the name parameter is not empty
     if (req.query.name && req.query.name.trim() !== '') {
-      query.name = { $regex: req.query.name, $options: "i" } // Using regex for partial match, case-insensitive
+      query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
     }
     // Add other filters to the query
     if (req.query.category) {
